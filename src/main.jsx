32cfd607120a07564efe267ce0f23a5a7b67fa06
Alejandro Ayuso Exposito <83668978+AlexAyuso01@users.noreply.github.com>
2025-10-31 13:03:08 +0100

    full, link updated
    full, link updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
-import App from '@/App';
-import '@/index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "@/App";
+import "@/index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
